2564c7482aac12546470624ec938ffae68cb01b5
vincent zhao <vincent.zhao@bigcommerce.com>
2016-01-22 23:00:07 +1100

    fix vegeta url generation on GET endpoint

--- sustain.go
+++ sustain.go
@@ -36,29 +36,30 @@ func sustain(opts *sustainOpts) error {
 	}
 
 	now := time.Now().Unix()
-	//for _, ep := range spec.Endpoints {
-	//var output []byte
-	//rate := fmt.Sprintf("--rate=%d", ep.RequestRate)
-	//script := fmt.Sprintf("--script=%s", ep.Script)
+	for _, ep := range spec.Endpoints {
+		var output []byte
+		rate := fmt.Sprintf("--rate=%d", ep.RequestRate)
+		script := fmt.Sprintf("--script=%s", ep.Script)
 
-	//if output, err = exec.Command("wrk2", "-t4", "-c100", "-d3m", rate, script, "--latency", ep.Url).Output(); err != nil {
-	//return err
-	//}
+		cmd := exec.Command("wrk2", "-t4", "-c100", "-d5m", rate, script, "--latency", ep.Url)
+		if output, err = cmd.Output(); err != nil {
+			return err
+		}
 
-	//file, err := os.Create(fmt.Sprintf("%s_%s_@%d_%d.hdrm", opts.outputPrefix, ep.Name, ep.RequestRate, now))
-	//if err != nil {
-	//return err
-	//}
+		file, err := os.Create(fmt.Sprintf("%s_%s_@%d_%d.hdrm", opts.outputPrefix, ep.Name, ep.RequestRate, now))
+		if err != nil {
+			return err
+		}
 
-	//_, err = file.Write(output)
-	//if err != nil {
-	//return err
-	//}
+		_, err = file.Write(output)
+		if err != nil {
+			return err
+		}
 
-	//// sleep a minute between each endpoint to give
-	//// already queued requests time to finish
-	//time.Sleep(time.Minute)
-	//}
+		// sleep a minute between each endpoint to give
+		// already queued requests time to finish
+		time.Sleep(time.Minute)
+	}
 
 	for i, ep := range spec.Endpoints {
 
@@ -73,7 +74,7 @@ func sustain(opts *sustainOpts) error {
 		workers := fmt.Sprintf("-workers=%d", ep.RequestRate)
 		output := fmt.Sprintf("-output=%s_%s_@%d_%d.bin", opts.outputPrefix, ep.Name, ep.RequestRate, now)
 
-		cmd := exec.Command("vegeta", "attack", targets, header, "-duration=3m", rate, workers, output)
+		cmd := exec.Command("vegeta", "attack", targets, header, "-duration=5m", rate, workers, output)
 		if err = cmd.Run(); err != nil {
 			return err
 		}
@@ -134,7 +135,7 @@ func writeTargetsFile(method, urls, bodies, targets string) error {
 		}
 	} else if method == "GET" || method == "HEAD" || method == "DELETE" {
 		for urlReader.Scan() {
-			if _, err = targetsFile.WriteString(urlReader.Text()); err != nil {
+			if _, err = targetsFile.WriteString(fmt.Sprintf("%s %s\n", method, urlReader.Text())); err != nil {
 				return err
 			}
 		}
